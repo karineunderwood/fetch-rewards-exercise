df0ba2b2f83000ba2aec9d1a40ee98179d8b599a
karineunderwood <karineunderwood23@gmail.com>
2022-11-07 14:28:27 -0800

    Working on spend route

--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 from flask import (Flask, render_template, request, flash,
                     session, redirect)
 import random
-from datetime import date
+from datetime import datetime
 from jinja2 import StrictUndefined
 import os
 
@@ -12,16 +12,58 @@ app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
+TRANSACTIONS = []
+
 @app.route("/")
-def homepage():
+def show_homepage():
 
-    return render_template('home.html')
+    return render_template("base.html")
+
+@app.route("/add-transactions")
+def show_options_to_add_points():
+    "Show form to enter points to add."
+
+    return render_template("add-transactions.html")
+
+@app.route("/add-transactions", methods=["POST"])
+def add_transactions():
+    """This is the route to add points from payers. """
+    if request.method == "POST":
+
+        payer = request.form.get("Payer")
+        points = request.form.get("Points") 
+
+        transaction = {"Payer": payer.upper(), "Point": points, "Timestamp": datetime.now()}
+        TRANSACTIONS.append(transaction)
+        print(TRANSACTIONS)
+
+    return render_template("add-transactions.html")
+
+    
+@app.route("/spend-points")
+def spend_points():
+    """This route shows form to spend points."""
+
+    return render_template("spend-points.html")
+
+@app.route("/spend-points", methods=["POST"])
+def store_points():
+    """This allows users to request points to spend."""
+    if request.method == "POST":
+        spend_points = request.form.get("Points-spend")
+
+        
+
+        print(TRANSACTIONS)
+    return render_template("spend-points.html")
+        
 
 
 
 
+    
 
-
+    
 
 
 
